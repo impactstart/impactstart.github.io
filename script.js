d8805f640800059bbe6c2f5719aa979c0c462171
impactstart <a.maritza.g@hotmail.com>
2025-03-21 15:05:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const email = document.getElementById('email').value;
 
-        fetch('https://tv7h471wj4.execute-api.us-west-1.amazonaws.com/EmailSubscription', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email: email })
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert(data.message); // Display success message
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('There was an error subscribing. Please try again later.');
-        });
+fetch('https://tv7h471wj4.execute-api.us-west-1.amazonaws.com/EmailSubscription', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: email })
+})
+.then(response => response.json())
+.then(data => {
+    alert(data.message); // Display success message
+})
+.catch(error => {
+    alert('There was an error subscribing. Please try again later.');
+});
+
     });
 });
