fccb65c0851496ab4f4ec84a8bb5b6bd844b6183
impactstart <a.maritza.g@hotmail.com>
2025-03-20 22:15:52 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('subscribeForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+
+  fetch('https://zh0zhjkuea.execute-api.us-east-2.amazonaws.com/EmailSubscription', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: email })
+  })
+  .then(response => response.json())
+  .then(data => {
+    alert(data.message); // Display success message
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('There was an error subscribing. Please try again later.');
+  });
+});
